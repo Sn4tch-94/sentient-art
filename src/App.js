5d819bc511accd7c3b7795d38814dce1b62b1ab2
Sn4tch <alexis.leclercq@epitech.eu>
2022-01-04 21:36:13 +0100

    Add sidebar + some fonts family

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
+
+import SideBar from './components/SideBar'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<Router>
+				<div className="app-sidebar">
+					<SideBar/>
+				</div>
+			</Router>
+		</div>
+		
+	);
 }
 
 export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,39 @@
+import './SideBar.css'
+import React from 'react'
+
+function SideBar() {
+	return (
+		<div className="sidebar-container">
+			<div className="sidebar-line"/>
+			<div className="sidebar-text">
+				S
+				<br/>
+				E
+				<br/>
+				N
+				<br/>
+				T
+				<br/>
+				I
+				<br/>
+				E
+				<br/>
+				N
+				<br/>
+				T
+				<br/>
+				
+				<br/>
+				A
+				<br/>
+				R
+				<br/>
+				T
+				<br/>
+			</div>
+			<div className="sidebar-line"/>
+		</div>
+	)
+}
+
+export default SideBar
