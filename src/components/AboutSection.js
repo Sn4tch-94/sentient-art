31a2d1d30143e31f72fa98f80aba332c054aebe1
Sn4tch <alexis.leclercq@epitech.eu>
2022-01-05 18:53:32 +0100

    Add about section to home page

---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import "./AboutSection.css"
+
+import tencent from '../ressources/tencent.png'
+import epic from '../ressources/epic.svg'
+import midgard from '../ressources/midgar.svg'
+import warp_frog from '../ressources/warp_frog.svg'
+import poliigon from '../ressources/poliigon.svg'
+
+function AboutSection() {
+	return (
+		<div className="about-container">
+			<div className="about-background-title">About</div>
+			<div className="about-title">About Sentient Art</div>
+			<div className="about-description">
+				Sentient Art is a AAA outsourcing studio specialized in Hard Surface models, 
+				Vehiclees, leveraging Real Time Rendering, driven by passion for life-like 
+				details and realistic modeling. The Sentient Art team has been selected with 
+				the utmost care for its exceptional work. Our attention to details enables us 
+				to stand out and offer high-quality standards.
+			</div>
+			<div className="about-client-logos">
+				<img className="about-client-logo" src={tencent} alt=""  />
+				<img className="about-client-logo" src={epic} alt=""  />
+				<img className="about-client-logo" src={midgard} alt=""  />
+				<img className="about-client-logo" src={warp_frog} alt=""  />
+				<img className="about-client-logo" src={poliigon} alt=""  />
+			</div>
+		</div>
+	)
+}
+
+export default AboutSection
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import "./Home.css"
 import HeroSection from "../components/HeroSection"
+import AboutSection from "../components/AboutSection"
 
 export default function Home() {
 	return (
 		<div className="home">
 			<HeroSection/>
+			<AboutSection/>
 		</div>
 	)
 }
