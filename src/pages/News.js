6d872cea23cb6ac16dad65562c4b52f716302aca
Sn4tch <alexis.leclercq@epitech.eu>
2022-01-07 18:50:11 +0100

    Fix wrong className of News

--- src/pages/News.js
+++ src/pages/News.js
@@ -2,10 +2,10 @@ import "./News.css"
 
 function News() {
 	return (
-		<div className="news-us">
-			<div className="news-us-container">
-				<div className="news-us-title">Coming soon</div>
-				<div className="news-us-body">This page is under maintenance, please come back in a few days.</div>
+		<div className="news">
+			<div className="news-container">
+				<div className="news-title">Coming soon</div>
+				<div className="news-body">This page is under maintenance, please come back in a few days.</div>
 			</div>
 		</div>
 	)
